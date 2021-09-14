7945145f7e637f2fb85b1eadb21d10bb3aff5752
Dieniffer <dieniffer.vargas@g.globo>
2021-09-14 15:33:00 -0300

    umaTasknaList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from "./components/Header"
 
 //aqui foi
 import { Container, TextField, IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import AddIcon from '@material-ui/icons/Add';
+import { Add as AddIcon } from '@material-ui/icons';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -17,11 +17,14 @@ const useStyles = makeStyles((theme) => ({
 function App() {
   const classes = useStyles();
 
+  const [list, setList] = useState([]);
+  const [task, setTask] = useState("");
+
 const handleChange = (event) => {  
   const { value } = event.target
-   console.log(value)
-  
+   setTask(value) 
 }
+
 
   return <body>
     <Header />
@@ -39,15 +42,21 @@ const handleChange = (event) => {
 
         <IconButton
           aria-label="newTask"
-          onClick={() => { alert('Ta indo UHUUULES') }}>
+          
+          // adicionar um novo valor ao estado anterior
+          onClick={() => setList([task])}>
+
           <AddIcon color="primary" fontSize="large" />
         
         </IconButton>
-       
-        
-       
       </form>
 
+
+      <h4>Lista de Tarefas</h4>
+      {/* percorrer a lista .map */}
+      <ul>  
+        <li> { list} </li>
+      </ul>
     </Container>
   </body>
 }
